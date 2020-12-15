149a9f58976cd4a6dd4baf12414a9b5cca800afb
Madhan Kandasamy <madhanmyil24@gmail.com>
2020-12-15 12:41:00 -0500

    Making Http Requests

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 
-import { Recipe } from './recipe.model';
+import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
